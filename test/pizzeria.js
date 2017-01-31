61352a8b37a86ad4b2a35eb33e3123dcadb81382
Samel Vhatargh <samel@bk.ru>
2017-01-31 22:55:03 +1100

    Выделяем фабрику для создания объектов тестирования

--- test/pizzeria.js
+++ test/pizzeria.js
@@ -6,26 +6,40 @@ var Pizzeria = require('../src/pizza/pizzeria');
 var Pizza = require('../src/pizza/pizza');
 var SystemDate = require('../src/pizza/systemdate');
 
+class PizzeriaTestsFactory {
+    createClient() {
+        return new Client(new Date(1943, 3, 3));
+    }
+    createPizzasForOrder() {
+        return [new Pizza('peperoni')];
+    }
+    createPizzeria() {
+        return new Pizzeria({'peperoni': 500});
+    }
+}
+
 
 describe('Pizzeria should', function () {
+    let factory =  new PizzeriaTestsFactory();
+    let me = factory.createClient();
+    let pizzeria = factory.createPizzeria();
+
     describe('give me pizza', function(){
         it('if I order pizza', function() {
-            let me = new Client();
-            let pizzas = [new Pizza('peperoni')];
-            let pizzeria = new Pizzeria({'peperoni': 500});
+            let pizzas = factory.createPizzasForOrder();
 
             let order = me.order(pizzas);
             pizzeria.serve(order, me);
 
-            assert.equal(pizzas, me.pizzas);
+            assert.equal('peperoni', me.pizzas[me.pizzas.length - 1].name);
+
+            me.pizzas = [];
         });
     });
     describe('give me special pizza', function(){
         it('if I order pizza at birthday', function() {
             (new SystemDate()).set(new Date(2016, 3, 3));
-            let me = new Client(new Date(1943, 3, 3));
-            let pizzas = [new Pizza('peperoni')];
-            let pizzeria = new Pizzeria({'peperoni': 500});
+            let pizzas = factory.createPizzasForOrder();
 
             let order = me.order(pizzas);
             pizzeria.serve(order, me);
@@ -35,9 +49,7 @@ describe('Pizzeria should', function () {
     });
     describe('give me 100$ discount', function(){
         it('if I order pizza with ABCD promocode', function() {
-            let me = new Client(null, '');
-            let pizzas = [new Pizza('peperoni')];
-            let pizzeria = new Pizzeria({'peperoni': 500});
+            let pizzas = factory.createPizzasForOrder();
 
             let order = me.order(pizzas, 'ABCD');
             pizzeria.serve(order, me);
